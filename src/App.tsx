1c1a437c53e805fed8b44f45d40587d02b800629
Nynke <nynke.van.schuppen@persgroep.net>
2022-04-08 13:47:42 +0200

    removed unnecessary code home module

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {Routes, Route} from "react-router-dom";
 import {Header} from './components/header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { SignIn } from './components/signin';
+import { Home } from './components/home';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header/>
       <Routes>
         <Route path="/signin" element={<SignIn/>}/>
+        <Route path="/home" element={<Home/>}/>
       </Routes>
     </div>
   );
