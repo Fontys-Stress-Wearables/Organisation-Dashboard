f1c9c94fce599881e5fd0726b937a9f98d4ea7d8
Nynke <nynke.van.schuppen@persgroep.net>
2022-04-21 16:00:36 +0200

    fix styling

--- src/components/caregivers/caregivers.tsx
+++ src/components/caregivers/caregivers.tsx
@@ -13,30 +13,28 @@ const Caregivers = () => {
     return(
         <div className={styles.container}>
             <div>
-            <div className={styles.actionBar}>
                 <div className={styles.searchbar}>
                 <Searchbar/>
                 </div>
-            </div>
-            <div className={styles.accordion}>
-                <Accordion defaultActiveKey="0">
-                {caregivers && caregivers.length ? (
-                    caregivers.map((c, index) =>(
-                    <Accordion.Item eventKey={index.toString()}>
-                        <Accordion.Header> {c.firstName} </Accordion.Header>
-                        <Accordion.Body>
-                        The caregiver is part of the following patient-groups: {c.patientGroups}. 
-                        Do you wish to edit the patientdata: {c.emailAddress}, {c.lastName}, {c.isGuest}
-                        </Accordion.Body>
-                    </Accordion.Item>
-                    ))
-                ) : (
-                    <div>
-                    <Alert variant="primary">No caregivers found</Alert>
-                    </div>
-                )}
-                </Accordion>
-            </div>
+                <div className={styles.accordion}>
+                    <Accordion defaultActiveKey="0">
+                    {caregivers && caregivers.length ? (
+                        caregivers.map((c, index) =>(
+                        <Accordion.Item eventKey={index.toString()}>
+                            <Accordion.Header> {c.firstName} </Accordion.Header>
+                            <Accordion.Body>
+                            The caregiver is part of the following patient-groups: {c.patientGroups}. 
+                            Do you wish to edit the patientdata: {c.emailAddress}, {c.lastName}, {c.isGuest}
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        ))
+                    ) : (
+                        <div>
+                        <Alert variant="primary">No caregivers found</Alert>
+                        </div>
+                    )}
+                    </Accordion>
+                </div>
             </div> 
      </div>
     );
--- src/components/patients/patients.tsx
+++ src/components/patients/patients.tsx
@@ -53,14 +53,12 @@ const Patients = () => {
     return(
        <div className={styles.container}>
           <div>
-            <div className={styles.actionBar}>
               <div className={styles.searchbar}>
                 <Searchbar/>
               </div>
               <div className={styles.createPatientModal}>
                 <CreatePatientModal/>
               </div>
-            </div>
             <div className={styles.accordion}>
               <Accordion defaultActiveKey="0">
                 {patients && patients.length ? (
