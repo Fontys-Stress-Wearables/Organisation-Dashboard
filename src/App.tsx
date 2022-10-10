70ad8ee3f479f1c5d560173bd1c6c8402eff7af9
Milan <58393068+Desoxyr@users.noreply.github.com>
2022-10-10 16:50:36 +0200

    Add linter and update code to new linting style

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import './App.css';
-import {Routes, Route, Navigate} from "react-router-dom";
-import {Header} from './components/header';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Patients } from './components/patients';
-import { Caregivers } from './components/caregivers';
-import { PatientGroups } from './components/patient-groups';
+import './App.css'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { Header } from './components/header'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Patients } from './components/patients'
+import { Caregivers } from './components/caregivers'
+import { PatientGroups } from './components/patient-groups'
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <Routes>
-        <Route path="/patients" element={<Patients/>}/>
-        <Route path="/caregivers" element={<Caregivers/>}/>
-        <Route path="/patient-groups" element={<PatientGroups/>}/>
-        <Route
-          path="*"
-          element={<Navigate to="/patients" replace />}
-        />
+        <Route path="/patients" element={<Patients />} />
+        <Route path="/caregivers" element={<Caregivers />} />
+        <Route path="/patient-groups" element={<PatientGroups />} />
+        <Route path="*" element={<Navigate to="/patients" replace />} />
       </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
